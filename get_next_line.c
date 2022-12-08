db9fae1158cc39cdd3aac1edabae501f506d271e
shinfray <simon@hinfray.eu>
2022-12-08 10:55:34 +0100

    Update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 19:28:41 by shinfray          #+#    #+#             */
-/*   Updated: 2022/12/07 19:33:54 by shinfray         ###   ########.fr       */
+/*   Updated: 2022/12/08 10:55:20 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 char	*get_next_line(int	fd)
 {
 	static char	**cache;
+	char		*line;
 
-
+	return (line);
 }
