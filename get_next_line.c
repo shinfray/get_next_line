9c154264932666c07fe5f32b1c243a0b152bbf64
shinfray <simon@hinfray.eu>
2022-12-20 09:42:36 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 21:20:03 by shinfray          #+#    #+#             */
-/*   Updated: 2022/12/19 18:30:10 by shinfray         ###   ########.fr       */
+/*   Updated: 2022/12/20 09:41:38 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,6 @@ char	*get_next_line(int fd)
 	char		*line;
 
 	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE < 1 || BUFFER_SIZE >= INT_MAX)
-		return (NULL);
 	line = NULL;
 	if (ft_retrieve_from_cache(&cache, &line) == NEWLINE_NOT_FOUND)
 		ft_parser(fd, &line, &cache);
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 18:25:15 by shinfray          #+#    #+#             */
-/*   Updated: 2022/12/19 18:25:18 by shinfray         ###   ########.fr       */
+/*   Updated: 2022/12/20 09:37:27 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 int	main(void)
 {
-	static int	array[INT_MAX];
+//	static int	array[INT_MAX];
+
+	printf("%lu", sizeof(char*));
 }
