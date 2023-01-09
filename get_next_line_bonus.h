c8cd8d25c3c0652d7427ffb8fd38415ef650bacf
Simon Hinfray <shinfray@student.s19.be>
2023-01-09 16:41:31 +0100

    update

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 18:23:19 by shinfray          #+#    #+#             */
-/*   Updated: 2023/01/09 16:38:32 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/01/09 16:41:20 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 # define GET_NEXT_LINE_BONUS_H
 
 # define ERROR				-1
-# define NEWLINE_FOUND		1
 # define NEWLINE_NOT_FOUND	0
+# define NEWLINE_FOUND		1
 
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
 
 char	*get_next_line(int fd);
-
 size_t	ft_gnl_strlen(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_gnl_strnjoin(char **s1, char const *s2, size_t n);
